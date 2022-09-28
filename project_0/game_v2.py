7bc5b52319f58767d1ff749a2e3dafc7a77c4f3d
kuzevanovsa <kuzevanovsa@gmail.com>
2022-09-28 14:44:22 +0500

    add predict func

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -24,6 +24,35 @@ def random_predict(number: int = 1) -> int:
     return count
 
 
+def predict(number: int = 1) -> int:
+    
+    """Function to predict a random number in 20 or less tries
+
+    Args:
+        number (int, optional): a predict number. Defaults to 1.
+        
+    Returns:
+        int: count of tries
+    """
+    
+    count = 0
+    min_number = 1
+    max_number = 100
+
+    while True:
+        count += 1
+        predict_number = (max_number - min_number + 1) // 2  # предполагаемое число
+        if number == predict_number:
+            break  # выход из цикла если угадали
+        elif number > predict_number:
+            # если угадываемое число больше предполагаемого, изменяем нижнюю границу
+            min_number = predict_number
+        else:
+            # если угадываемое число меньше предполагаемого, изменяем верхнюю границу
+            max_number = predict_number
+    return count
+
+
 def score_game(random_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
@@ -42,9 +71,9 @@ def score_game(random_predict) -> int:
 
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
-    return score
+    return score 
 
 
 if __name__ == "__main__":
     # RUN
-    score_game(random_predict)
+    score_game(predict)
